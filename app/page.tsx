fe51d41d35f3ffac33cd790dd9c77b03705feb6b
Mauricio <mgp.94@hotmail.com>
2025-10-03 13:56:21 -0500

    Repazo de los dos primeros capitulos

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
+import styles from '@/app/ui/home.module.css';
 //import HeroImage from '@/app/ui/hero-image';
 import Image from 'next/image';
 
@@ -12,6 +13,9 @@ export default function Page() {
       </div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
+          <div
+            className={styles.shape}
+          />
           <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal`}>
             <strong>Welcome to Acme.</strong> This is the example for the{' '}
             <a href="https://nextjs.org/learn/" className="text-blue-500">
@@ -27,15 +31,15 @@ export default function Page() {
           </Link>
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
-         {/* <img src="/hero-desktop.png" alt="Hero" className="hidden md:block" /> 
+          {/* <img src="/hero-desktop.png" alt="Hero" className="hidden md:block" /> 
           <HeroImage />*/}
-          <Image src="/hero-desktop.png" alt="Hero" className="hidden md:block" 
-          width={1000}
-          height={760}
+          <Image src="/hero-desktop.png" alt="Hero" className="hidden md:block"
+            width={1000}
+            height={760}
           />
-           <Image src="/hero-mobile.png" alt="Hero" className="block md:hidden" 
-          width={560}
-          height={620}
+          <Image src="/hero-mobile.png" alt="Hero" className="block md:hidden"
+            width={560}
+            height={620}
           />
 
 
