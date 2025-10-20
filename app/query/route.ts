2e1a786357373ce76e9a4f7ab074a09ca76753a7
Mauricio <mgp.94@hotmail.com>
2025-10-20 13:45:44 -0500

    Commit de prueba

--- app/query/route.ts
+++ app/query/route.ts
@@ -1,13 +1,13 @@
 import postgres from 'postgres';
 
-const sql = postgres(process.env.POSTGRES_URL!, { {ssl: 'require' });
+const sql = postgres(process.env.POSTGRES_URL!,({ssl: 'require' }));
 
  async function listInvoices() {
  	const data = await sql`
     SELECT invoices.amount, customers.name
      FROM invoices
     JOIN customers ON invoices.customer_id = customers.id
-    WHERE invoices.amount = 666;
+    WHERE invoices.amount = 676;
    `;
 
 	return data;
